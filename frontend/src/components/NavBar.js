c1ff52318956a259f06696757bd6ea7b9895dcd2
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-04-14 14:43:10 +0200

    mark active tab

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,35 +1,57 @@
-import React, { Component } from "react"
+import React from "react"
 import { withRouter } from "react-router-dom"
 
 import "../styles/NavBar.css"
 
-class NavBar extends Component {
-  redirect(target) {
-    return () => this.props.history.push(target)
+function NavBar(props) {
+  function redirect(path) {
+    return () => {
+      props.history.push(path)
+    }
   }
 
-  render() {
-    return (
-      <div className="navbar">
-        <div className="navbar-item-container">
-          <div className="name" onClick={this.redirect("/")}>
-            {/* <span>Logo</span> */}
-            <span>Reddalert</span>
-          </div>
-          <div className="navbar-item" onClick={this.redirect("/")}>
-            Home
-          </div>
-          <div className="navbar-item" onClick={this.redirect("/login")}>
-            Login
-          </div>
-          <div className="navbar-item" onClick={this.redirect("/register")}>
-            Register
-          </div>
-        </div>
-        <div className="color-line" />
-      </div>
-    )
+  function isActive(page) {
+    switch (props.location.pathname) {
+      case "/":
+        return page === "home"
+      case "/login":
+        return page === "login"
+      case "/register":
+        return page === "register"
+      default:
+        break
+    }
   }
+
+  return (
+    <div className="navbar">
+      <div className="navbar-item-container">
+        <div className="name" onClick={redirect("/")}>
+          {/* <span>Logo</span> */}
+          <span>Reddalert</span>
+        </div>
+        <div
+          className={`navbar-item ${isActive("home") ? "active" : ""}`}
+          onClick={redirect("/")}
+        >
+          Home
+        </div>
+        <div
+          className={`navbar-item ${isActive("login") ? "active" : ""}`}
+          onClick={redirect("/login")}
+        >
+          Login
+        </div>
+        <div
+          className={`navbar-item ${isActive("register") ? "active" : ""}`}
+          onClick={redirect("/register")}
+        >
+          Register
+        </div>
+      </div>
+      <div className="color-line" />
+    </div>
+  )
 }
 
 export default withRouter(NavBar)
