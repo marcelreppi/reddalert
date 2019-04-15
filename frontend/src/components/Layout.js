7892591b07aacaf0dba10daa122ce64b88cf734e
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-04-15 13:05:24 +0200

    added footer, contact page, privacy policy page

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react"
+import React from "react"
+import { Link } from "react-router-dom"
 
 import NavBar from "./NavBar"
 
 import "../styles/Layout.css"
 
-class Layout extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <div className="main-content-container">{this.props.children}</div>
+function Layout(props) {
+  return (
+    <div className="layout">
+      <NavBar />
+      <div className="content-container">{props.children}</div>
+      <div className="footer">
+        <Link to="/contact">Contact</Link>
+        <Link to="/privacy">Privacy Policy</Link>
+        <div>
+          Got any question? Send an email to{" "}
+          <a href="mailto:support@reddalert.me">support@reddalert.me</a>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Layout
---
+++ frontend/src/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const Contact = () => (
+  <Layout>
+    <div className="page-title">Contact us!</div>
+    <p>
+      Send an email to{" "}
+      <a href="mailto:support@reddalert.me">support@reddalert.me</a>
+    </p>
+    <p>We will be in touch with you in no time!</p>
+  </Layout>
+)
+
+export default Contact
